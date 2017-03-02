52742af257e2bb3d8f71bd72d9fc9e3abe04a0f2
elysewarren <eawdesigns@aol.com>
2017-03-02 09:22:06 -0500

    Commiting after completing project.

--- app/src/main/java/com/elysewarren/inclassassignment05_ew/Main2Activity.java
+++ app/src/main/java/com/elysewarren/inclassassignment05_ew/Main2Activity.java
@@ -1,14 +1,15 @@
 package com.elysewarren.inclassassignment05_ew;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView textType;
     private TextView textAge;
     private TextView textInfo;
+    private TextView textAllergies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +18,20 @@ public class Main2Activity extends AppCompatActivity {
         textType = (TextView) findViewById(R.id.display_dog_type);
         textAge = (TextView) findViewById(R.id.display_dog_age);
         textInfo = (TextView) findViewById(R.id.display_contact_info);
+        textAllergies = (TextView) findViewById(R.id.display_allergies);
 
 
         Intent intent = getIntent();
-        Dog dog= (Dog) intent.getSerializableExtra(Keys.DOG);
+        Dog dog = (Dog) intent.getSerializableExtra(Keys.DOG);
 
-        textType.setText(dog.toString());
+        textType.setText("Type: " + dog.getDogType());
+        textAge.setText("Age: " + String.valueOf(dog.getAge()));
+        textInfo.setText("Contact Information: " + dog.getContactInfo());
 
+        if (dog.isAllergies())
+            textAllergies.setText("Is allergic");
+        else
+            textAllergies.setText("Not allergic");
     }
 
 
